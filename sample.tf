782143e3e672dd51faa29e3a257e7d14d4ee96d9
sgururaj2021 <gururaj1931@gmail.com>
2025-08-29 12:24:37 +0530

    again changes

--- sample.tf
+++ sample.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "Instance Built Using Terraform" {
-  ami           = ami-09c813fb71547fc4f
+  ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
 
   tags = {
